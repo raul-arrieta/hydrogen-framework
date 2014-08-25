a83f826cd36706afb7e4f4fa2fa0b6b5b4fbca4e
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-08-25 17:09:10 +0200

    Added the possibility of getting data from URL directly

--- source/js/hydrogen.data.js
+++ source/js/hydrogen.data.js
@@ -8,16 +8,32 @@ hydrogen.data = (function () {
         if (typeof (dataConfiguration) === "function") {
 
             // We need to run the funciton for getting data
-            dataConfiguration(function (result) {
+            dataConfiguration(function (data) {
 
                 $.ajax({ url: template }).
                     done(function (templateHtml) {
 
-                        var dataTemplated = Mustache.render(templateHtml, result);
+                        var dataTemplated = Mustache.render(templateHtml, data);
                         $container.html(dataTemplated);
 
                     });
             });
+
+        } else if (typeof (dataConfiguration) === "string") {
+
+            // It is a path. We should get data from that URL path and merge it with the template
+
+            $.ajax({ url: dataConfiguration }).
+                done(function (data) {
+
+                    $.ajax({ url: template }).
+                        done(function (templateHtml) {
+
+                            var dataTemplated = Mustache.render(templateHtml, data);
+                            $container.html(dataTemplated);
+
+                        });
+                });
 
         } else {
             // It is an Array, so we already have data
