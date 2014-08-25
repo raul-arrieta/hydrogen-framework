76ea917456a143ee1d8f82b64c166c809b94c3a2
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-08-25 16:33:38 +0200

    Lint corrections

--- source/js/hydrogen.data.js
+++ source/js/hydrogen.data.js
@@ -1,15 +1,17 @@
-/*global hydrogen, Mustache */
-hydrogen.data = (function(){
+/*global hydrogen, Mustache, $ */
+hydrogen.data = (function () {
 
-    var proccessDataWithTemplate = function(dataConfiguration, template, $container){
+    "use strict";
 
-        if(typeof(dataConfiguration) === "function"){
+    var proccessDataWithTemplate = function (dataConfiguration, template, $container) {
+
+        if (typeof (dataConfiguration) === "function") {
 
             // We need to run the funciton for getting data
-            dataConfiguration(function(result){
+            dataConfiguration(function (result) {
 
                 $.ajax({ url: template }).
-                    done(function(templateHtml) {
+                    done(function (templateHtml) {
 
                         var dataTemplated = Mustache.render(templateHtml, result);
                         $container.html(dataTemplated);
@@ -17,12 +19,11 @@ hydrogen.data = (function(){
                     });
             });
 
-        }
-        else{
+        } else {
             // It is an Array, so we already have data
 
             $.ajax({ url: template }).
-                done(function(templateHtml) {
+                done(function (templateHtml) {
 
                     var dataTemplated = Mustache.render(templateHtml, dataConfiguration);
                     $container.html(dataTemplated);
--- source/js/hydrogen.routes.js
+++ source/js/hydrogen.routes.js
@@ -1,71 +1,73 @@
 /*global hydrogen */
-hydrogen.routes = (function(){
+hydrogen.routes = (function () {
+
+    "use strict";
 
     var routeTable = [],
 
         templateBasePath = "",
 
-        templateExtension = "";
+        templateExtension = "",
 
-    var add = function(configuration){
+        add = function (configuration) {
 
-        var newRoute = {};
+            var newRoute = {};
 
-        if(configuration.url){
+            if (configuration.url) {
 
-            newRoute.url = configuration.url;
+                newRoute.url = configuration.url;
 
-        }
-
-        if(configuration.container){
-
-            newRoute.container = configuration.container;
-
-        }
-
-        if(configuration.template){
-
-            newRoute.template = configuration.template;
-
-        }
-
-        if(configuration.data){
-
-            newRoute.data = configuration.data;
-
-        }
-
-        routeTable.push(newRoute);
-    };
-
-    var navigateTo = function(url){
-
-        for(var routeCounter = 0; routeCounter < routeTable.length; routeCounter++){
-
-            if(routeTable[routeCounter].url === url){
-
-                if(routeTable[routeCounter].container){
-
-                    var templateUrl = this.templateBasePath + routeTable[routeCounter].template + this.templateExtension,
-                        $container = $("#" + routeTable[routeCounter].container),
-                        dataSource = routeTable[routeCounter].data;
-
-                    if(routeTable[routeCounter].data){
-
-                        hydrogen.data.proccessDataWithTemplate(dataSource, templateUrl, $container);
-
-                    }
-                    else{
-
-                        $("#" + routeTable[routeCounter].container).load(templateUrl);
-
-                    }
-                }
-
-                break;
             }
-        }
-    };
+
+            if (configuration.container) {
+
+                newRoute.container = configuration.container;
+
+            }
+
+            if (configuration.template) {
+
+                newRoute.template = configuration.template;
+
+            }
+
+            if (configuration.data) {
+
+                newRoute.data = configuration.data;
+
+            }
+
+            routeTable.push(newRoute);
+        },
+
+        navigateTo = function (url) {
+
+            for (var routeCounter = 0, length = routeTable.length; routeCounter < length; routeCounter++){
+
+                if(routeTable[routeCounter].url === url){
+
+                    if(routeTable[routeCounter].container){
+
+                        var templateUrl = this.templateBasePath + routeTable[routeCounter].template + this.templateExtension,
+                            $container = $("#" + routeTable[routeCounter].container),
+                            dataSource = routeTable[routeCounter].data;
+
+                        if(routeTable[routeCounter].data){
+
+                            hydrogen.data.proccessDataWithTemplate(dataSource, templateUrl, $container);
+
+                        }
+                        else{
+
+                            $("#" + routeTable[routeCounter].container).load(templateUrl);
+
+                        }
+                    }
+
+                    break;
+                }
+            }
+        };
 
     return {
 
