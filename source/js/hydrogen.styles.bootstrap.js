e017dc82a9f1799a1ef6fb217e4e385620e58ced
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-08-27 21:14:36 +0200

    Rewrite: cleanup and added styles for container

--- source/js/hydrogen.styles.bootstrap.js
+++ source/js/hydrogen.styles.bootstrap.js
@@ -12,7 +12,14 @@ hydrogen.styles.bootstrap = (function () {
            $("head").append("<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/united/bootstrap.min.css'>");
         },
 
-        applyStyles = function(){
+
+        _applyStylesToTopBody = function(){
+
+            $("body").wrapInner("<div class='container'></div>");
+
+        },
+
+        _applyStylesToTopMenu = function(){
 
             var $menuContainer = $("div[data-control='top-menu']"),
 
@@ -36,55 +43,14 @@ hydrogen.styles.bootstrap = (function () {
             $("div[data-reference='menu-container']", $menuContainer).append($menuItemsContainer);
 
             $menuContainer.wrap("<nav class='navbar navbar-default' role='navigation'></nav>");
-            /*
 
-            <nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
+        },
 
+        applyStyles = function(){
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+            _applyStylesToTopBody();
 
-
-            */
+            _applyStylesToTopMenu();
 
         };
 
