1dcd75ecccfbe34d0ee6fc046d08e5890be58b42
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-08-27 21:16:17 +0200

    Added option for page title

--- source/js/hydrogen.styles.bootstrap.js
+++ source/js/hydrogen.styles.bootstrap.js
@@ -23,7 +23,9 @@ hydrogen.styles.bootstrap = (function () {
 
             var $menuContainer = $("div[data-control='top-menu']"),
 
-                $menuItemsContainer = $("ul", $menuContainer).first();
+                $menuItemsContainer = $("ul", $menuContainer).first(),
+
+                brandName = $menuContainer.attr("data-brand") || "Title";
 
             $menuContainer.
                 addClass("container-fluid").
@@ -34,7 +36,7 @@ hydrogen.styles.bootstrap = (function () {
                     <span class='icon-bar'></span>\
                     <span class='icon-bar'></span>\
                   </button>\
-                  <a class='navbar-brand' href='#'>Brand</a>\
+                  <a class='navbar-brand' href='#'>" + brandName + "</a>\
                 </div>\
                 <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1' data-reference='menu-container'></div>");
 
