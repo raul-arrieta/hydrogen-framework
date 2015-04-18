a9a22ca9ae13f863a98296cf99105ead8c60c147
Raul Arrieta Brouard <raul.arrieta@gmail.com>
2015-04-19 00:32:57 +0200

    Adding passport identification

---
+++ spec/HydrogenAuthentication.js
@@ -0,0 +1,13 @@
+/*
+describe('HydrogenAuthentication', function() {
+  var context = {};
+
+  beforeEach(function() {
+    //nothing for now
+  });
+
+  it('should work', function() {
+    
+  });
+
+});*/
